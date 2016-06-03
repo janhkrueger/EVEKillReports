4d90f9e32b0fe0c94e1291850de3354f712d2c57
Jan H. Krüger <janhkrueger@gmail.com>
2016-06-03 10:30:09 +0200

    Create createKR_participantsinvalidHash.sql

---
+++ killmails/createKR_participantsinvalidHash.sql
@@ -0,0 +1,4 @@
+CREATE TABLE KR_participantsinvalidHash (
+  killID int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO KR_participantsinvalidHash VALUES (18206302);
