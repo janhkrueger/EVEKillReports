0d48e54e9871b47027159587abc4855f857fcc57
Jan H. Krüger <janhkrueger@gmail.com>
2016-06-03 09:59:57 +0200

    Create createKR_characterUpdatesWork.sql

---
+++ characters/createKR_characterUpdatesWork.sql
@@ -0,0 +1,4 @@
+CREATE TABLE KR_characterUpdatesWork (
+  characterID bigint(20) NOT NULL,
+  PRIMARY KEY (characterID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=16;
