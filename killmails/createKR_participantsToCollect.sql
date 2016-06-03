162db9aef3e7a4da682e3935ca58e2559af7787a
Jan H. Krüger <janhkrueger@gmail.com>
2016-06-03 10:31:34 +0200

    Create createKR_participantsToCollect.sql
    Controlling which shipTypes to migrate from the old table to the new one.

---
+++ killmails/createKR_participantsToCollect.sql
@@ -0,0 +1,4 @@
+CREATE TABLE KR_participantsToCollect (
+  shipGroupName varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (shipGroupName)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
