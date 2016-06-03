d6528af5ec61d1930ca0eccd95ddc3e86248f4e7
Jan H. Krüger <janhkrueger@gmail.com>
2016-06-03 09:58:46 +0200

    Create createKR_characterUpdates.sql

---
+++ characters/createKR_characterUpdates.sql
@@ -0,0 +1,13 @@
+CREATE TABLE KR_characterUpdates (
+  characterID bigint(20) NOT NULL,
+  characterName text COLLATE utf8mb4_unicode_ci,
+  corporationID bigint(11) DEFAULT NULL,
+  corporationName text COLLATE utf8mb4_unicode_ci,
+  allianceID bigint(11) DEFAULT NULL,
+  allianceName text COLLATE utf8mb4_unicode_ci,
+  lastUpdate datetime DEFAULT NULL,
+  PRIMARY KEY (characterID),
+  KEY characterID (characterID,lastUpdate),
+  KEY corpID (characterID,corporationID),
+  KEY allianceID (characterID,allianceID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=16;
