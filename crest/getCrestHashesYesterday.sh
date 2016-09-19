d1fa0bd76686bdd1c5f5de336e86be3bb50fce07
Jan H. Krüger <janhkrueger@gmail.com>
2016-09-19 22:03:49 +0200

    Adding bro compression and storing

--- crest/getCrestHashesYesterday.sh
+++ crest/getCrestHashesYesterday.sh
@@ -4,6 +4,7 @@
 JETZT="$(date "+%Y%m%d" -d "1 days ago")"
 FILENAME=$JETZT.json.gz
 FILEJSON=$JETZT.json
+FILEBRO=$JETZT.json.bro
 
 # get the gzip compressed crest dump and unzip.
 wget 'https://zkillboard.com/api/history/'$JETZT'/' --https-only --header='Accept-Encoding: gzip' -O /var/games/KillReporter/crest/$FILENAME
@@ -11,4 +12,6 @@ gunzip $FILENAME
 
 # import the saved dump, delete afterwars
 /usr/bin/python2.7 /var/games/KillReporter/crest/importCrestHashes.py $FILEJSON
+/var/brotli/tools/bro --quality 10 --input $FILEJSON --output $FILEBRO
+mv -f $FILEBRO data/
 rm $FILEJSON
