d2a12e9a7326cd544fea05e3b71f88a470c95bc8
Jan H. Krüger <janhkrueger@gmail.com>
2016-09-19 21:38:44 +0200

    short scripts to import the crest dumps from zKillboard.com

---
+++ crest/getCrestHashesYesterday.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+JETZT="$(date "+%Y%m%d" -d "1 days ago")"
+FILENAME=$JETZT.json.gz
+FILEJSON=$JETZT.json
+
+echo $JETZT
+wget 'https://zkillboard.com/api/history/'$JETZT'/' --https-only --header='Accept-Encoding: gzip' -O /var/games/KillReporter/crest/$FILENAME
+gunzip $FILENAME
+/usr/bin/python2.7 /var/games/KillReporter/crest/importCrestHashes.py $FILEJSON
+rm $FILEJSON
