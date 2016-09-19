837491d8f53a9ba6d9a36bd365f2814a716b9bc2
Jan H. Krüger <janhkrueger@gmail.com>
2016-09-19 21:41:15 +0200

    Update getCrestHashes7DaysBefore.sh

--- crest/getCrestHashes7DaysBefore.sh
+++ crest/getCrestHashes7DaysBefore.sh
@@ -1,10 +1,14 @@
 #! /bin/sh
+
+# calculate the timestamp from 7 days before
 JETZT="$(date "+%Y%m%d" -d "7 days ago")"
 FILENAME=$JETZT.json.gz
 FILEJSON=$JETZT.json
 
-echo $JETZT
+# get the gzip compressed crest dump and unzip.
 wget 'https://zkillboard.com/api/history/'$JETZT'/' --https-only --header='Accept-Encoding: gzip' -O /var/games/KillReporter/crest/$FILENAME
 gunzip $FILENAME
+
+# import the saved dump, delete afterwars
 /usr/bin/python2.7 /var/games/KillReporter/crest/importCrestHashes.py $FILEJSON
 rm $FILEJSON
