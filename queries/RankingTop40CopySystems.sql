b3d11770e80de6a623f60e0710654fa86addcaf0
Jan H. Krüger <janhkrueger@gmail.com>
2016-09-26 01:07:05 +0200

    Create RankingTop40CopySystems.sql

---
+++ queries/RankingTop40CopySystems.sql
@@ -0,0 +1,12 @@
+SELECT ci.solarSystemName, mr.regionName, round((sum(ci.copyIndex)*100),2) as sumcopyindex, round(avg(mss.security),2) as avgsecurity, IF( avg(mss.security) >=0.5, 'HighSec', IF( avg(mss.security) <=0.0, 'NullSec', 'LowSec')) as SystemSecurity, ci.surveyDate
+FROM industryCostIndexes ci
+	JOIN mapSolarSystems mss
+	ON (ci.solarSystemID = mss.solarSystemID)
+	JOIN mapRegions mr
+	ON (mr.regionID = mss.regionID)
+-- WHERE mr.regionName = 'Domain'
+WHERE surveyDate = ( select max(c2.surveyDate) FROM industryCostIndexes c2 )
+-- AND ci.solarSystemName = 'Alkabsi'
+GROUP BY ci.solarSystemName
+ORDER BY sumcopyindex desc
+LIMIT 0,40
