b062676f20098f546b1f8333ed3ed11ba2f9c132
Jan H. Krüger <janhkrueger@gmail.com>
2016-09-25 23:51:51 +0200

    Create fortizarKillsBySecurityState.sql

---
+++ queries/fortizarKillsBySecurityState.sql
@@ -0,0 +1,18 @@
+SELECT 
+  p.killID, 
+  p.solarSystemID, 
+  mss.solarSystemName AS solarSystemName, 
+  round(avg(mss.security),2) AS avgsecurity,
+  if((avg(mss.security) >= 0.5),'HighSec',if((avg(mss.security) <= 0.0),'NullSec','LowSec')) AS SystemSecurity 
+FROM 
+  KR_participants PARTITION (p2016) p, 
+  mapSolarSystems mss,
+  mapRegions mr 
+WHERE 
+  p.shipTypeID = 35833 AND 
+  p.isVictim = 1 AND 
+  p.solarSystemID = mss.solarSystemID AND 
+  mr.regionID = mss.regionID
+group by 
+  mss.solarSystemName, killID
+order by SystemSecurity
