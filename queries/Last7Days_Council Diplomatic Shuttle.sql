293c100b6d1bc15aa0a62368e2c5913d712a9d59
Jan H. Krüger <janhkrueger@gmail.com>
2016-09-26 01:05:16 +0200

    Create Last7Days_Council Diplomatic Shuttle.sql

---
+++ queries/Last7Days_Council Diplomatic Shuttle.sql
@@ -0,0 +1,8 @@
+select * 
+from KR_participants 
+where (
+	(KR_participants.shipTypeID in (34496)) 
+	and (KR_participants.isVictim = 1) 
+	and (KR_participants.kill_time <= now()) 
+	and (KR_participants.kill_time >= (cast(now() as date) - interval 7 day))) 
+order by KR_participants.kill_time
