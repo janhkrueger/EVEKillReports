07b0b5e8961c8e572fc4281d93f301ca2ca364a2
Jan H. Krüger <janhkrueger@gmail.com>
2016-09-26 01:03:04 +0200

    Create Drifter TotalValue by ShipGroup.sql

---
+++ queries/Drifter TotalValue by ShipGroup.sql
@@ -0,0 +1,8 @@
+select ig.groupName as ShipClass, sum(p1.totalValue) as ISKValue
+from KR_participants p1, invTypes it, invGroups ig
+where ( (p1.isVictim = 1) and p1.killID in (select p2.killID from KR_participants p2 where ((p2.characterID in (3019581,3019582)) 
+and (p2.finalBlow = 1)
+)))
+AND it.typeID = p1.shipTypeID
+AND ig.groupID = it.groupID
+GROUP BY ig.groupName
