e60cbd4e727543b6e6a4cb04fbcf6f28b7031769
Jan H. Krüger <janhkrueger@gmail.com>
2016-10-12 00:25:22 +0200

    Create loadFolder.sh

---
+++ crest/data/loadFolder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# loading all cresthashes of a year and then archiving the entire files
+# uses facebooks zStandard algo for compressing
+# JHK, 20161003
+
+for f in 2015/*.json; do
+    ./loadCrestHashes.py $f
+done
+tar cfv 2015.tar 2015/
+zstd --ultra -22 -r 2015.tar
+rm 2015.tar
+rm -R 2015/
