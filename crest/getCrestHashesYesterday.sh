4d1e4da8c3bc817363c0390be88dcfe7c39851c9
Jan H. Krüger <janhkrueger@gmail.com>
2016-10-12 00:32:32 +0200

    Update getCrestHashesYesterday.sh

--- crest/getCrestHashesYesterday.sh
+++ crest/getCrestHashesYesterday.sh
@@ -1,17 +1,12 @@
 #! /bin/sh
-
-# calculate the timestamp from last day
 JETZT="$(date "+%Y%m%d" -d "1 days ago")"
 FILENAME=$JETZT.json.gz
 FILEJSON=$JETZT.json
 FILEBRO=$JETZT.json.bro
 
-# get the gzip compressed crest dump and unzip.
 wget 'https://zkillboard.com/api/history/'$JETZT'/' --https-only --header='Accept-Encoding: gzip' -O /var/games/KillReporter/crest/$FILENAME
 gunzip $FILENAME
-
-# import the saved dump, delete afterwars
-/usr/bin/python2.7 /var/games/KillReporter/crest/importCrestHashes.py $FILEJSON
+/usr/bin/python2.7 /var/games/KillReporter/crest/loadCrestHashes.py $FILEJSON
 /var/brotli/tools/bro --quality 10 --input $FILEJSON --output $FILEBRO
 mv -f $FILEBRO data/
 rm $FILEJSON
