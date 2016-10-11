a97194fdf84043de4f13fad45f99d16d790a60fd
Jan H. Krüger <janhkrueger@gmail.com>
2016-10-12 00:30:35 +0200

    Create getCrestHashes30DaysBefore.sh

---
+++ crest/getCrestHashes30DaysBefore.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+JETZT="$(date "+%Y%m%d" -d "30 days ago")"
+FILENAME=$JETZT.json.gz
+FILEJSON=$JETZT.json
+FILEBRO=$JETZT.json.bro
+
+wget 'https://zkillboard.com/api/history/'$JETZT'/' --https-only --header='Accept-Encoding: gzip' -O /var/games/KillReporter/crest/$FILENAME
+gunzip $FILENAME
+/usr/bin/python2.7 /var/games/KillReporter/crest/loadCrestHashes.py $FILEJSON
+/var/brotli/tools/bro --quality 10 --input $FILEJSON --output $FILEBRO
+mv -f $FILEBRO data/
+rm $FILEJSON
