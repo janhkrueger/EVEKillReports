932c00f693ae3ead35527cefcde30a35816dd99e
Jan H. Krüger <janhkrueger@gmail.com>
2016-10-20 00:27:05 +0200

    Update addCharIDsToWatch.sh
    Changing script to use credentials from .my.cnf

--- characters/addCharIDsToWatch.sh
+++ characters/addCharIDsToWatch.sh
@@ -4,13 +4,11 @@ echo "#      Begin     #"
 echo "##################"
 
 # MySQL Variablen bereitstellen
-dbuser=
-dbpass=
 dbname=
 limit=50000000
 
 echo "# Neue CharacterIDs aufnehmen"
-mysql -u$dbuser -p$dbpass $dbname -B -e "INSERT INTO KR_characterUpdates (characterid) SELECT distinct p.characterid FROM KR_participants p WHERE p.characterid NOT IN (select c.characterID FROM KR_characterUpdates c) limit $limit"
+mysql $dbname -B -e "INSERT INTO KR_characterUpdates (characterid) SELECT distinct p.characterid FROM KR_participants p WHERE p.characterid NOT IN (select c.characterID FROM KR_characterUpdates c) limit $limit"
 echo "##################"
 echo "# Ende           #"
 echo "##################"
