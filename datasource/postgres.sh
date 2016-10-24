30d99fe9af0099d77de8695ddddc9d45bd514a90
Jan H. Krüger <janhkrueger@gmail.com>
2016-10-24 12:23:23 +0200

    Updating from server

--- datasource/postgres.sh
+++ datasource/postgres.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-wget https://www.fuzzwork.co.uk/dump/latest/postgres-20160429-TRANQUILITY-2-schema.dmp.bz2
-bunzip2 postgres-20160429-TRANQUILITY-2-schema.dmp.bz2
-pg_restore  -U --role=p -d  postgres-20160429-TRANQUILITY-2-schema.dmp
-rm postgres-20160429-TRANQUILITY-2-schema.dmp.bz2
+wget https://www.fuzzwork.co.uk/dump/postgres-latest.dmp.bz2
+bunzip2 postgres-latest.dmp.bz2
+pg_restore -U  --role= -d eve postgres-latest.dmp
+rm postgres-latest.dmp
