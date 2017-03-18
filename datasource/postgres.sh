775681b1bd62f659406443f2922a0816bdb5bcd9
Jan H. Krüger <janhkrueger@gmail.com>
2017-03-18 23:51:00 +0100

    Preparing sde for future activity

--- datasource/postgres.sh
+++ datasource/postgres.sh
@@ -3,3 +3,24 @@ wget https://www.fuzzwork.co.uk/dump/postgres-latest.dmp.bz2
 bunzip2 postgres-latest.dmp.bz2
 pg_restore -U  --role= -d eve postgres-latest.dmp
 rm postgres-latest.dmp
+
+# Exporting dump
+pg_dump -Ueve_user eve_db --data-only --no-owner --column-inserts --no-security-labels --no-tablespaces --no-unlogged-table-data --schema=public > test.sql
+
+# Remove comments
+sed -i '/^--/d' test.sql
+
+# Remove comments
+sed -i '/^SET/d' test.sql
+
+# Remove empty lines
+sed -i '/^\s*$/d' test.sql
+
+# Remove double quotes
+sed -ie 's/"//g' test.sql
+
+# Adding schema
+sed -i 's/INSERT INTO /INSERT INTO sde./g' test.sql
+
+#Running vacuum on the tables
+psql -U eve_user -t -A -c "select 'VACUUM '||table_schema||'.'||table_name||';' from information_schema.tables where table_schema = 'sde'" eve_db | psql -U eve_user eve_db
